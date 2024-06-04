fe7e21ca259ecbfeacf8e7f4a8264ce72fb6d75c
Victor Samuel <samuelvictoralexander@gmail.com>
2024-06-04 21:45:31 +0100

    u

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -21,8 +21,7 @@ const Projects = () => {
       image: "/img/image7.webp",
       title: "VictorMart",
       desc: "An E-Commerce  Application  built with Next.js 13.4, TailwindCSS, DaisyUi Typescript, Prisma and  MongoDB",
-      liveUrl:
-        "https://victor-mart-4ocurqekr-bobleeswagger09s-projects.vercel.app",
+      liveUrl: "https://victor-mart.vercel.app",
       repo: "https://github.com/Bobleeswagger09/VictorMart",
     },
     {
